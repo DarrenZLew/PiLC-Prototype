74a0cf3431b12b6c9840f3097678c4a1976f475d
Darren Lew <darrenzlew@gmail.com>
2018-03-05 01:43:22 -0600

    Add routing logic. Add LiveFeed/Events components.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import React, { Component } from "react";
-import { GlobalHeader } from "./components/GlobalHeader";
-import Components from "./components/Components";
+import GlobalHeader from "./components/GlobalHeader";
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-
-    };
-  };
+    this.state = {};
+  }
 
   render() {
     return (
       <div className="App">
         <GlobalHeader />
-        <Components />
       </div>
     );
   }
--- src/components/GlobalHeader.jsx
+++ src/components/GlobalHeader.jsx
@@ -1,28 +1,53 @@
-import React from "react";
+import React, { Component } from "react";
 import { Nav, Navbar, NavItem } from "react-bootstrap";
+import { Switch, Route } from "react-router-dom";
+import { LinkContainer } from "react-router-bootstrap";
+import { withRouter } from "react-router-dom";
 
-export const GlobalHeader = () => {
-  return (
-    <Navbar inverse collapseOnSelect fixedTop>
-      <Navbar.Header>
-        <Navbar.Brand>
-          <a href="#brand">PiLC-Prototype</a>
-        </Navbar.Brand>
-        <Navbar.Toggle />
-      </Navbar.Header>
-      <Navbar.Collapse>
-        <Nav pullRight>
-          <NavItem eventKey={1} href="#">
-            Live Feed
-          </NavItem>
-          <NavItem eventKey={2} href="#">
-            Components
-          </NavItem>
-          <NavItem eventKey={3} href="#">
-            Events
-          </NavItem>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
-  )
-};
+import Components from "./Components";
+import LiveFeed from "./LiveFeed";
+import Events from "./Events";
+
+class GlobalHeader extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar inverse collapseOnSelect fixedTop>
+          <Navbar.Header>
+            <Navbar.Brand>PiLC-Prototype</Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav pullRight>
+              <LinkContainer to="/livefeed">
+                <NavItem eventKey={1}>Live Feed</NavItem>
+              </LinkContainer>
+              <LinkContainer to="/components">
+                <NavItem eventKey={2}>Components</NavItem>
+              </LinkContainer>
+              <LinkContainer to="/events">
+                <NavItem eventKey={3}>Events</NavItem>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <RoutingPaths />
+      </div>
+    );
+  }
+}
+
+const RoutingPaths = () => (
+  <Switch>
+    <Route path="/livefeed" component={LiveFeed} />
+    <Route path="/components" component={Components} />
+    <Route path="/events" component={Events} />
+  </Switch>
+);
+
+export default withRouter(GlobalHeader);
